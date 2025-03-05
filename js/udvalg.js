6ea04bbdac2ba34431f59316eb7e8322da0ec083
idakjrg <idadk7@gmail.com>
2025-03-05 10:26:52 +0100

    hej
    hej

--- js/udvalg.js
+++
@@ -1,7 +0,0 @@
-function toggleDropdown() {
-    let box = document.getElementById("beerBox");
-    let button = document.querySelector(".dropdown-btn");
-
-    box.classList.toggle("show");
-    button.classList.toggle("open");
-}
